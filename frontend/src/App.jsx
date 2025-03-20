a34a4b6ec917e35d58bd4866032648571f98d684
anshu-kumar27 <anshukumar3552@gmail.com>
2025-03-20 12:31:24 +0530

    deployment changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-// import Loading from './componenets/loading/Loading'
 import MainBody from './componenets/MainBody'
-import { BrowserRouter } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
         <MainBody/>
-    </BrowserRouter>
   )
 }
 
